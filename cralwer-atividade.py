7a98d171682f7dfe0fc79f717372f5396f58ab5a
Khaio <kaio.fonseca@decisaosistemas.com.br>
2023-10-10 14:26:00 -0300

    feat: bd

--- cralwer-atividade.py
+++ cralwer-atividade.py
@@ -1,9 +1,14 @@
-#imports
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
 
 # URL da Netflix
 netflix_url = "https://www.netflix.com/br/browse/genre/34399"
+
+client = MongoClient("localhost", 27017)  # Conecta ao MongoDB local
+  # Conecta ao MongoDB local
+db = client["netflix_catalog"]  # Nome do banco de dados
+collection = db["movies"]  # Nome da coleção
 
 # Função para fazer a solicitação e analisar a página da Netflix
 def crawl_netflix_catalog():
@@ -15,17 +20,15 @@ def crawl_netflix_catalog():
         if response.status_code == 200:
             # Analisar o conteúdo HTML da página usando BeautifulSoup
             soup = BeautifulSoup(response.content, 'html.parser')
-            
+
             # Encontrar todos os elementos que contêm informações de filmes
             movie_elements = soup.find_all('section', class_='nm-collections-row')
-            #print(movie_elements)
 
             # Iterar pelos elementos e extrair títulos e artwork
             for movie in movie_elements:
-                # Extraindo o 
-                categoria = movie.find('h2',  class_ = 'nm-collections-row-name').text
+                # Extraindo o
+                categoria = movie.find('h2',  class_='nm-collections-row-name').text
                 title = movie.find('span', class_='nm-collections-title-name').text
-                
 
                 # Extraindo a URL da artwork (imagem)
                 artwork = movie.find('img', class_='nm-collections-title-img')['src']
@@ -36,11 +39,42 @@ def crawl_netflix_catalog():
                 print("Artwork:", artwork)
                 print("\n")
 
+                # Inserir os dados no MongoDB
+                movie_data = {
+                    "categoria": categoria,
+                    "titulo": title,
+                    "artwork": artwork
+                }
+                result = collection.insert_one(movie_data)
+                print(result)
+                if result != None:
+                    print("Dados inseridos com sucesso no MongoDB.")
+
         else:
             print("Falha ao acessar a página da Netflix")
 
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
 
-# Chamar a função para iniciar o web crawler
+# Função para buscar dados no MongoDB
+def buscar_filme_por_titulo(titulo):
+    try:
+        # Realizar a busca no MongoDB
+        movie = collection.find_one({"titulo": titulo})
+
+        if movie:
+            print("Dados encontrados:")
+            print("Categoria:", movie["categoria"])
+            print("Título:", movie["titulo"])
+            print("Artwork:", movie["artwork"])
+        else:
+            print("Filme não encontrado no catálogo.")
+
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+
+# Chamar a função para iniciar o web crawler e inserir os dados no MongoDB
 crawl_netflix_catalog()
+
+# Chamar a função de busca por título
+buscar_filme_por_titulo("The Queen's Gambit")
